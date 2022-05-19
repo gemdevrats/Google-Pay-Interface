ac77e371978640fe2934517c8be4a3d6f179beeb
gemdevrats <devvrat.sharma@geminisoloution.com>
2022-05-19 16:57:36 +0530

    updated transaction Id

--- src/app/hard-coded-payment-data.service.ts
+++ src/app/hard-coded-payment-data.service.ts
@@ -7,10 +7,10 @@ import { Payment } from './payment';
 export class HardCodedPaymentDataService {
   paymentData : Payment[] = [];
   constructor() {
-    this.paymentData.push(new Payment('Pay','Sent','ADS456754356',2000,'You Paid',new Date('03/17/2021')));
+    this.paymentData.push(new Payment('Pay','Sent','ADS4567543564588753465',2000,'You Paid',new Date('03/17/2021')));
     this.paymentData.push(new Payment('Pay','Received','',1500,'Request Received',new Date('06/23/2021')));
     this.paymentData.push(new Payment('Collect','Received','',250,'You Requested',new Date('08/12/2021')));
-    this.paymentData.push(new Payment('Collect','Sent','ADS456754356',5000,'You Received',new Date('02/09/2022')));
+    this.paymentData.push(new Payment('Collect','Sent','ADS456754356767879878',5000,'You Received',new Date('02/09/2022')));
     this.paymentData.push(new Payment('Pay','Received','',4500,'Request Received',new Date('04/23/2022')));
    }
 
